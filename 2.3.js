916c396e18a014c6917d486317dd82a9d24cbfb6
Da-nae <grosjean.danae@gmail.com>
2022-10-23 15:18:51 +0200

    fixed the bonus

--- 2.3.js
+++ 2.3.js
@@ -1,16 +1,21 @@
 // Display all even numbers between 1 and 100.
 // Bonus: do two different versions. The first one should use while and make a loop that will execute 100 times. The second one should use for and make a loop that will execute 50 times.
 
-/* With a while loop 
+// With a while loop 
 let i = 2;
 
 while (i <= 100) {
-    console.log(i);
-    i += 2;
+    if(i%2 == 0){
+        console.log(i);
+        i++;
+    } else {
+        i++;
+    }
 }
-*/
 
-// Bonus : with a for loop
+
+/** Bonus : with a for loop
 for (let i = 2; i <= 100; i += 2) {
     console.log(i);
-}
+} 
+*/
